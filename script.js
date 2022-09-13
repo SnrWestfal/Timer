8f9892a1dcb7871131f14594e08b3f201d3723e6
Westfal <77891009+SnrWestfal@users.noreply.github.com>
2022-09-13 15:02:27 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const countDown = document.getElementById("countdown");
+
+let time = 3600;
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? "0" + seconds: seconds;
+    countDown.innerHTML = `${minutes}: ${seconds}`;
+    time--;
+}
